5d702917335414e73813b12a5ddea07b48501ac0
Олег Федорович <oleg@timepad.ru>
2021-03-12 12:46:29 +0300

    АСТ-210 Изменен способ импорта react

--- src/components/button/SliderButton.tsx
+++ src/components/button/SliderButton.tsx
@@ -1,4 +1,5 @@
-import React, {MouseEventHandler, useState} from 'react';
+import * as React from 'react';
+import {MouseEventHandler, useState} from 'react';
 import {component} from '../../services/helpers/classHelpers';
 
 export interface ISliderButtonProps {
