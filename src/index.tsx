da1cd3dea2ebd3e745732ab2b4751a6dc18d1ec5
Rail Khusnutdinov <mail@rail-ka.ru>
2021-03-18 11:28:16 +0300

    PL-197 Миграция компонента Userpics

--- src/index.tsx
+++ src/index.tsx
@@ -8,6 +8,8 @@ import {
     IButtonProps,
 } from './components/button';
 
+export {Orgpic, Userpic} from './components/TPUI/Userpic';
+
 import {component} from './services/helpers/ClassHelper';
 
 export type {ISliderButtonProps, IButtonProps};
