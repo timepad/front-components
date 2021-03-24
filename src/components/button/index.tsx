616a0b11936c6f550f44e5d4319bf85f88a98f12
ru-web-designer <ru.web.designer@gmail.com>
2021-03-24 16:22:17 +0300

    MON-82 reverted button exports

--- src/components/button/index.tsx
+++ src/components/button/index.tsx
@@ -1,2 +1,6 @@
-export * from './Button';
-export * from './SliderButton';
+import {Button, ButtonVariant, ButtonIconAlignment, FixedVariant, IButtonProps} from './Button';
+import {SliderButton, ISliderButtonProps} from './SliderButton';
+
+export type {ISliderButtonProps, IButtonProps};
+
+export {Button, ButtonVariant, ButtonIconAlignment, FixedVariant, SliderButton};
