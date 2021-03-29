91d5f50548319c26d74c583405b339a4171418b3
Anatoly <anatoly@garpix.com>
2021-03-29 17:36:30 +0300

    PL-201 - Добавлен RadioGroup

---
+++ src/components/Forms/RadioButton/RadioGroup.tsx
@@ -0,0 +1,62 @@
+import React, {useState} from 'react';
+import cx from 'classnames';
+import {molecule} from '../../../services/helpers/classHelpers';
+
+import {RadioButton} from './RadioButton';
+
+export interface IRadioConfig {
+    title: string;
+    value: string;
+}
+
+export interface IRadioGroupConfig {
+    inputs: IRadioConfig[];
+}
+
+interface IRadioResult {
+    [key: string]: string;
+}
+
+interface IRadioGroupProps {
+    config: IRadioGroupConfig;
+    name: string;
+    label?: string;
+    error?: string;
+    touched?: boolean;
+    defaultSelected?: string;
+    onChange?: (result: IRadioResult) => void;
+    onBlurHandler?: (event: React.FormEvent<HTMLInputElement>) => void;
+}
+
+export function RadioGroup(props: IRadioGroupProps) {
+    const {config, name, label, error, touched, defaultSelected = null, onChange, onBlurHandler} = props;
+
+    const [selectedValue, setSelectedValue] = useState<string | null>(defaultSelected);
+
+    const formLabelClasses = molecule('form', 'label')({error: !!error && touched});
+
+    const labelClasses = cx('t-small', 't-color-gray', formLabelClasses);
+
+    const onChangeHandler = (name: string, value: string) => {
+        setSelectedValue(value);
+        onChange?.({[name]: value});
+    };
+
+    return (
+        <div>
+            <div className="lbrick" />
+            {label && <span className={labelClasses}>{label}</span>}
+            {config.inputs.map((input, idx) => (
+                <RadioButton
+                    key={idx}
+                    name={name}
+                    label={input.title}
+                    value={input.value}
+                    checked={selectedValue === input.value}
+                    onChangeHandler={(_, value) => onChangeHandler(name, value)}
+                    onBlurHandler={onBlurHandler}
+                />
+            ))}
+        </div>
+    );
+}
