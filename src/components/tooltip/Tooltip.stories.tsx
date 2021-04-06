9285b3a3fe01fff42856e8df55e572b7ff36491d
andrewglotov <a.glotov@timepad.ru>
2021-04-06 14:12:13 +0500

    правки в сториз

--- src/components/tooltip/Tooltip.stories.tsx
+++ src/components/tooltip/Tooltip.stories.tsx
@@ -4,6 +4,8 @@ import {Meta} from '@storybook/react/types-6-0';
 import 'css/bundle.less';
 import {StoryTitle} from '../../services/helpers/storyBookHelpers';
 import {Tooltip} from './Tooltip';
+import {Button, ButtonVariant} from '../button';
+import AddIcon from 'svg/24/icon-plus-24.svg';
 
 interface IStorybookComponent extends FC {
     storyName?: string;
@@ -19,32 +21,36 @@ export const Simple: IStorybookComponent = () => {
         <>
             <StoryTitle>First Tooltip Example</StoryTitle>
             <p>
-                <Tooltip message={'Текст в котором подробно рассказанопроисходящем на экране'} style={{color: 'red'}}>
-                    Tooltip
+                <h2> Реферат по философии</h2>
+                <br />
+                <b>Тема: «Почему поразительно освобождение?»</b>
+                <br />
+                Конфликт, как следует из{' '}
+                <Tooltip message={'Подсказка к тексту'} style={{color: 'red'}}>
+                    вышесказанного
                 </Tooltip>
-                Объемный текст в тексте
-            </p>
-
-            <p>
-                Объемный текст в котором
+                , прост. Ощущение мира категорически контролирует сложный бабувизм. Врожденная интуиция откровенна.
+                Актуализация, следовательно, контролирует субъективный принцип восприятия. Дуализм преобразует
+                сенсибельный катарсис. Позитивизм заполняет знак. Концепция, как принято считать, нетривиальна.
                 <Tooltip
-                    message={'Oбъемный текст в котором подробно рассказанопроисходящем на экране'}
+                    message={
+                        'Позитиви́зм (фр. positivisme, от лат. positivus — положительный) — философское учение и направление в методологии науки, определяющее единственным источником истинного, действительного знания эмпирические исследования и отрицающее познавательную ценность философского исследования.'
+                    }
                     style={{color: 'red'}}
                 >
-                    Tooltip
+                    Позитивизм
                 </Tooltip>
-                Объемный подробно рассказан о происходящем на экране
+                ясен не всем.{' '}
+                <Tooltip message={'Бхутавад - какой-то чувак'} style={{color: 'red'}}>
+                    Бхутавад
+                </Tooltip>
+                , следовательно, принимает во внимание типичный смысл жизни.
             </p>
 
-            <p>
-                Объемный текст в котором подробно рассказан о происходящем на экране
-                <Tooltip
-                    message={'Oбъемный текст в котором подробно рассказанопроисходящем на экране'}
-                    style={{color: 'red'}}
-                >
-                    Tooltip
-                </Tooltip>
-            </p>
+            <StoryTitle>Second Tooltip Example</StoryTitle>
+            <Tooltip message={'Текст в котором подробно рассказанопроисходящем на экране'} style={{color: 'red'}}>
+                <Button variant={ButtonVariant.primary} label="Click me" icon={<AddIcon />} />
+            </Tooltip>
         </>
     );
 };
