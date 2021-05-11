e2f92d275ecc578691f2b5245b1cbf72f35f8960
Rail Khusnutdinov <mail@rail-ka.ru>
2021-05-11 10:06:19 +0300

    move Logo component

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,4 @@
 export * from './components/button';
 export * from './components/tabs';
 export * from './services/helpers/classHelpers';
-export {Logo} from './components/Logo'
+export * from './components/logo';
