161277c599f199e2ac687064cc1caca08a1b824c
Rail Khusnutdinov <mail@rail-ka.ru>
2021-06-04 10:37:09 +0300

    fix Logo after PR review

--- src/components/logo/index.ts
+++ src/components/logo/index.ts
@@ -1,5 +1,5 @@
 export * from './Logo';
 import LogoMobile from '../../assets/svg/logo/logo.svg';
-import LogoDesktop from '../../assets/svg/logo/logo-desktop.svg';
+import LogoDesktop from '../../assets/svg/logo/logo-big.svg';
 import LogoShort from '../../assets/svg/logo/logo-short.svg';
 export {LogoMobile, LogoDesktop, LogoShort};
