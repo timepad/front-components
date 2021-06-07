888d3493be874a6ef76b084f9884a815c971c4f4
Anatoly <anatoly@garpix.com>
2021-06-07 18:05:16 +0300

    PL-202 - ref для Group

--- src/components/list/Group.tsx
+++ src/components/list/Group.tsx
@@ -14,56 +14,63 @@ export interface IItem {
     active?: boolean;
 }
 
-const Group: React.FC<IItem | any> = ({
-    children,
-    as: Tag = 'div',
-    className = '',
-    secondaryText,
-    prefix,
-    suffix,
-    label,
-    header,
-    active,
-    ...props
-}: IItem): JSX.Element => {
-    const Prefix = prefix,
-        Suffix = suffix,
-        classNames = cx(
-            component('list-item')({
-                'has-prefix': !!Prefix,
-                'has-suffix': !!Suffix,
-                header,
-                active,
-                'secondary-text': !!secondaryText,
-            }),
-            className,
-        );
+const Group: React.FC<IItem | any> = React.forwardRef<HTMLDivElement, IItem>(
+    (
+        {
+            children,
+            as: Tag = 'div',
+            className = '',
+            secondaryText,
+            prefix,
+            suffix,
+            label,
+            header,
+            active,
+            ...props
+        }: IItem,
+        ref,
+    ): JSX.Element => {
+        const Prefix = prefix,
+            Suffix = suffix,
+            classNames = cx(
+                component('list-item')({
+                    'has-prefix': !!Prefix,
+                    'has-suffix': !!Suffix,
+                    header,
+                    active,
+                    'secondary-text': !!secondaryText,
+                }),
+                className,
+            );
 
-    const mainText = label ? label : children;
-    return (
-        <div className={classNames}>
-            {Prefix ? (
-                <div className={component('list-item', 'prefix')()}>
-                    <Prefix />
-                </div>
-            ) : null}
-            <Tag className={component('list-item', 'tag')()} {...props}>
-                {secondaryText ? (
-                    <div>
-                        <div>{mainText}</div>
-                        <div className={component('list-item', 'secondary-text')()}>{secondaryText}</div>
+        const mainText = label ? label : children;
+        return (
+            <div ref={ref} className={classNames}>
+                {Prefix ? (
+                    <div className={component('list-item', 'prefix')()}>
+                        <Prefix />
                     </div>
-                ) : (
-                    <div>{mainText}</div>
-                )}
-            </Tag>
-            {Suffix ? (
-                <div className={component('list-item', 'suffix')()}>
-                    <Suffix />
-                </div>
-            ) : null}
-        </div>
-    );
-};
+                ) : null}
+                <Tag className={component('list-item', 'tag')()} {...props}>
+                    {secondaryText ? (
+                        <div>
+                            <div>{mainText}</div>
+                            <div className={component('list-item', 'secondary-text')()}>{secondaryText}</div>
+                        </div>
+                    ) : (
+                        <div>{mainText}</div>
+                    )}
+                </Tag>
+                {Suffix ? (
+                    <div className={component('list-item', 'suffix')()}>
+                        <Suffix />
+                    </div>
+                ) : null}
+            </div>
+        );
+    },
+);
+
+Group.displayName = 'Group';
 
 export {Group};
