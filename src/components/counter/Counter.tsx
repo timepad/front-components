7c3de7a668b91e6084542a8d53e8ce866abd849c
lzhabo <lidia.zhabo99@gmail.com>
2021-07-14 00:16:15 +0300

    Исправлены комментарии

--- src/components/counter/Counter.tsx
+++ src/components/counter/Counter.tsx
@@ -1,21 +1,18 @@
 import * as React from 'react';
 import './index.less';
 import {component} from '../../services/helpers/classHelpers';
+import {cn} from '@bem-react/classname';
 
 export interface ICounterProps {
     value?: number | string;
 }
 
+const cbadge = cn('cbadge');
+
 export const Counter: React.FC<ICounterProps> = ({value, children}) => {
     return (
         <div className={component('counter-wrapper')()}>
-            <div
-                className={component('badge-bg')({
-                    empty: value == null,
-                })}
-            >
-                <div className={component('badge')()}>{value}</div>
-            </div>
+            <div className={cbadge(value == null ? 'empty' : '')}>{value}</div>
             {children}
         </div>
     );
