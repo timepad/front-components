20404ecdb60017afed61595e3f05be0a9651cf0a
Ilya Dostoynov <ilyadostoynov@gmail.com>
2021-07-19 13:39:37 +0300

    TPI-192 исправлены иморты

--- src/index.tsx
+++ src/index.tsx
@@ -7,4 +7,5 @@ export * from './components/logo';
 export * from './components/dropdown';
 export * from './components/list';
 export * from './components/forms';
+export * from './components/loader';
 export * as Forms from './components/forms';
