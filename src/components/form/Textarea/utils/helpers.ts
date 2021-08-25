cbf23c19d9c960afcda1f2445fb490a25ca2088b
a.volchunovich <a.volchunovich@timepad.ru>
2021-08-25 19:44:49 +0300

    pl-374 install front-shared, added eslint disabled for Textarea helpers

--- src/components/form/Textarea/utils/helpers.ts
+++ src/components/form/Textarea/utils/helpers.ts
@@ -1,6 +1,7 @@
 // eslint-disable-next-line @typescript-eslint/no-empty-function,@typescript-eslint/explicit-module-boundary-types
 export const noop = () => {};
 
+// eslint-disable-next-line  @typescript-eslint/no-explicit-any
 export const pick = <Obj extends {[key: string]: any}, Key extends keyof Obj>(props: Key[], obj: Obj): Pick<Obj, Key> =>
     props.reduce((acc, prop) => {
         acc[prop] = obj[prop];
