37e0632cf08ec02c763bdb31e1bc1071251ac30e
pgalkevich <pgalkevich@gmail.com>
2021-08-26 13:13:07 +0300

    TPI-235 рефакторинг

--- src/components/logo/Logo.tsx
+++ src/components/logo/Logo.tsx
@@ -2,29 +2,24 @@ import * as React from 'react';
 import LogoSVG from '../../assets/svg/logo/logo.svg';
 import {component} from '../../services/helpers/classHelpers';
 import './index.less';
-import {ReactElement} from 'react';
 
 type Colors = 'gray' | 'white' | 'blue' | 'purple';
+type Actions = 'expandable' | 'hoverable';
 
 export interface ILogoProps {
     short?: boolean;
-    action?: string;
+    action?: Actions | '';
     color?: Colors | '';
 }
 
 export const Logo: React.FC<ILogoProps> = ({short, action, color}) => {
     const className = component('logo')();
 
-    let result: ReactElement = <LogoSVG className={color ?? ''} />;
-    if (short && action) {
-        result = (
+    return (
+        <div className={className}>
             <div className={action}>
-                <LogoSVG className={color ?? ''} />
+                <LogoSVG className={color ?? ''} width={short ? 12 : 107} />
             </div>
-        );
-    } else if (short) {
-        result = <LogoSVG width={12} className={color ?? ''} />;
-    }
-
-    return <div className={className}>{result}</div>;
+        </div>
+    );
 };
