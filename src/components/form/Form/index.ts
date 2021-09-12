86e737cbc9d6e5f0f39937646ad69bd45baf6229
a.volchunovich <a.volchunovich@timepad.ru>
2021-09-12 17:02:43 +0300

    pl-422 поправлен экспорт компонентов

--- src/components/form/Form/index.ts
+++ src/components/form/Form/index.ts
@@ -1,3 +1,7 @@
 import {Form} from './Form';
+import {TextLight, TextLightField} from '../TextLight';
+import {Text, TextField} from '../Text';
+import {Radio, RadioField} from '../Radio';
+import {Checkbox, CheckboxField} from '../Checkbox';
 
-export {Form};
+export {Form, TextField, TextLightField, TextLight, RadioField, Radio, Text, CheckboxField, Checkbox};
