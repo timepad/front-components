d2de326334402a3a262d2c222826ba3721f0b5ed
a.volchunovich <a.volchunovich@timepad.ru>
2021-09-12 17:43:45 +0300

    pl-422 fix props

--- src/components/form/Text/Text.types.ts
+++ src/components/form/Text/Text.types.ts
@@ -1,6 +1,15 @@
-import React from 'react';
+import React, {ChangeEventHandler} from 'react';
 import {ITextareaProps} from '../Textarea/Textarea';
 
 export type IFormTextProps = {
     error?: string;
-} & (({multiline?: false} & React.InputHTMLAttributes<HTMLInputElement>) | ({multiline: true} & ITextareaProps));
+} & (
+    | ({
+          multiline?: false;
+          onChange: ChangeEventHandler<HTMLInputElement>;
+      } & React.InputHTMLAttributes<HTMLInputElement>)
+    | ({
+          multiline: true;
+          onChange: ChangeEventHandler<HTMLTextAreaElement>;
+      } & ITextareaProps)
+);
