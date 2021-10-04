f7655ff52e11edcf915382fa3a7424483fb99d37
Ilya Dostoynov <ilyadostoynov@gmail.com>
2021-10-04 17:54:17 +0300

    TPI-196 Исправления после ревью

--- src/components/banner/Banner.stories.tsx
+++ src/components/banner/Banner.stories.tsx
@@ -23,12 +23,12 @@ export const Default: IStorybookComponent = () => {
                 You will need to enable two-factor or multi-factor authentication with your identity provider instead.
             </Banner>
             <StoryTitle>With Icon</StoryTitle>
-            <Banner withIcon>
+            <Banner icon>
                 Note: If you login to Figma via Google SSO or SAML SSO, you wont be able to enable two-factor in Figma.
                 You will need to enable two-factor or multi-factor authentication with your identity provider instead.
             </Banner>
             <StoryTitle>With custom Icon & Closable</StoryTitle>
-            <Banner closeable icon={IconEdit}>
+            <Banner closeable icon={<IconEdit />}>
                 Note: If you login to Figma via Google SSO or SAML SSO, you wont be able to enable two-factor in Figma.
                 You will need to enable two-factor or multi-factor authentication with your identity provider instead.
             </Banner>
--- src/components/banner/Banner.tsx
+++ src/components/banner/Banner.tsx
@@ -9,41 +9,23 @@ import {Button, ButtonVariant} from '../button';
 interface IProps {
     theme?: string;
     closeable?: boolean;
-    withIcon?: boolean;
-    icon?: React.FC<React.SVGProps<SVGSVGElement>>;
+    icon?: React.ReactElement<React.SVGProps<SVGSVGElement>> | boolean;
 }
 
-export const Banner: React.FC<IProps> = (props) => {
+export const Banner: React.FC<IProps> = ({children, closeable = false, icon}) => {
     const [show, setShow] = useState(true);
     if (!show) {
         return null;
     }
-    const {children, closeable, icon, withIcon} = props;
-
-    const Icon = () => {
-        if (props.icon) {
-            return <props.icon />;
-        }
-        if (withIcon) {
-            return <InfoIcon />;
-        }
-
-        return null;
-    };
-
-    // TODO: Временное решение. Исправить в будующем в самом row. Сделать row мод multiline.
-    const BodyStyle = {paddingLeft: icon ? 0 : '16px', paddingRight: closeable ? 0 : '16px'};
 
     return (
         <Row className={component('banner')()}>
             {icon && (
                 <Row.Icon className={component('banner', 'icon')()}>
-                    <Icon />
+                    <BannerIcon icon={icon} />
                 </Row.Icon>
             )}
-            <Row.Body style={BodyStyle} className={component('banner', 'content')()}>
-                {children}
-            </Row.Body>
+            <Row.Body className={component('banner', 'content')()}>{children}</Row.Body>
             {closeable && (
                 <Row.Icon className={component('banner', 'icon')()}>
                     <Button onClick={() => setShow(false)} variant={ButtonVariant.transparent} icon={<CloseIcon />} />
@@ -51,4 +33,14 @@ export const Banner: React.FC<IProps> = (props) => {
             )}
         </Row>
     );
+};
+
+const BannerIcon: React.FC<{icon: React.ReactElement<React.SVGProps<SVGSVGElement>> | boolean}> = ({icon}) => {
+    if (icon) {
+        if (typeof icon === 'boolean') {
+            return <InfoIcon />;
+        }
+        return icon;
+    }
+    return null;
 };
