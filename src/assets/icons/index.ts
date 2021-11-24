3bbc15ad25af6359c28d15b5d8b56c375a87d34e
mauta <anna.opareva@gmail.com>
2021-11-24 11:56:56 +0300

    TPI-324 поменяли названия иконок

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -1,121 +1,120 @@
-import IconArrowDSolid16 from './16/icon-Arrow-D-solid-16.svg';
-import IconArrowRSolid16 from './16/icon-Arrow-R-solid-16.svg';
-import IconArrowSolid16 from './16/icon-arrow-solid-16.svg';
-import IconArrowSortingDown16 from './16/icon-arrow-sorting-down-16.svg';
-import IconArrowSortingUp16 from './16/icon-arrow-sorting-up-16.svg';
-import IconArrowUSolid16 from './16/icon-Arrow-U-solid-16.svg';
-import IconBullet16 from './16/icon-bullet-16.svg';
-import IconCalendar16 from './16/icon-calendar-16.svg';
-import IconCheckmark16 from './16/icon-checkmark-16.svg';
-import IconCloseAlt16 from './16/icon-close-alt-16.svg';
-import IconDislikeOutline16 from './16/icon-dislike_outline-16.svg';
-import IconDrag16 from './16/icon-drag-16.svg';
-import IconEdit16 from './16/icon-edit-16.svg';
-import IconFile16 from './16/icon-file-16.svg';
-import IconFile216 from './16/icon-file2-16.svg';
-import IconHide16 from './16/icon-hide-16.svg';
-import IconInfo16 from './16/icon-info-16.svg';
-import IconLikeOutline16 from './16/icon-like_outline-16.svg';
-import IconLoading16 from './16/icon-loading-16.svg';
-import IconMinus16 from './16/icon-minus-16.svg';
-import IconModer16 from './16/icon-moder-16.svg';
-import IconOpenLink16 from './16/icon-open-link-16.svg';
-import IconPin16 from './16/icon-pin-16.svg';
-import IconPlus16 from './16/icon-plus-16.svg';
-import IconView16 from './16/icon-view-16.svg';
-import IconAttendees24 from './24/icon-attendees-24.svg';
-import IconDocument24 from './24/icon-document-24.svg';
-import IconHome24 from './24/icon-home-24.svg';
-import IconAddCalendar24 from './24/icon-add-calendar-24.svg';
-import IconAlert24 from './24/icon-alert-24.svg';
-import IconApplePay24 from './24/icon-apple-pay-24.svg';
-import IconArrowNW24 from './24/icon-arrow_NW-24.svg';
-import IconArrowTale24 from './24/icon-arrow_tale-24.svg';
-import IconArrowTaleCurve24 from './24/icon-arrow_tale-curve-24.svg';
-import IconArrowDown24 from './24/icon-arrow-down-24.svg';
-import IconArrowLeft24 from './24/icon-arrow-left-24.svg';
-import IconArrowRight24 from './24/icon-arrow-right-24.svg';
-import IconArrowUp24 from './24/icon-arrow-up-24.svg';
-import IconBell24 from './24/icon-bell-24.svg';
-import IconBookmarkS24 from './24/icon-bookmark_s-24.svg';
-import IconBookmark24 from './24/icon-bookmark-24.svg';
-import IconCalendar24 from './24/icon-calendar-24.svg';
-import IconCard24 from './24/icon-card-24.svg';
-import IconCheckCircle24 from './24/icon-check_circle-24.svg';
-import IconCheck24 from './24/icon-check-24.svg';
-import IconChevronDown24 from './24/icon-chevron-down-24.svg';
-import IconChevronLeft24 from './24/icon-chevron-left-24.svg';
-import IconChevronRight24 from './24/icon-chevron-right-24.svg';
-import IconChevronUp24 from './24/icon-chevron-up-24.svg';
-import IconCloseCircle24 from './24/icon-close_circle-24.svg';
-import IconClose24 from './24/icon-close-24.svg';
-import IconCopy24 from './24/icon-copy-24.svg';
-import IconDelete24 from './24/icon-delete-24.svg';
-import IconDrag24 from './24/icon-drag-24.svg';
-import IconEdit24 from './24/icon-edit-24.svg';
-import IconEllipsis24 from './24/icon-ellipsis-24.svg';
-import IconEnterFullscreen24 from './24/icon-enter_fullscreen-24.svg';
-import IconEnter24 from './24/icon-enter-24.svg';
-import IconExitFullscreen24 from './24/icon-exit_fullscreen-24.svg';
-import IconFacebook24 from './24/icon-facebook-24.svg';
-import IconInfo24 from './24/icon-info-24.svg';
-import IconInstagram24 from './24/icon-instagram-24.svg';
-import IconLink24 from './24/icon-link-24.svg';
-import IconLoading24 from './24/icon-loading-24.svg';
-import IconLock24 from './24/icon-lock-24.svg';
-import IconLogin24 from './24/icon-login-24.svg';
-import IconLogout24 from './24/icon-logout-24.svg';
-import IconMenu24 from './24/icon-menu-24.svg';
-import IconMinusCircle24 from './24/icon-minus_circle-24.svg';
-import IconMinus24 from './24/icon-minus-24.svg';
-import IconPhoto24 from './24/icon-photo-24.svg';
-import IconPin24 from './24/icon-pin-24.svg';
-import IconPlayS24 from './24/icon-play_s-24.svg';
-import IconPlay24 from './24/icon-play-24.svg';
-import IconPlusCircle24 from './24/icon-plus_circle-24.svg';
-import IconPlus24 from './24/icon-plus-24.svg';
-import IconProfile24 from './24/icon-profile-24.svg';
-import IconQiwi24 from './24/icon-qiwi-24.svg';
-import IconQrcode24 from './24/icon-qrcode-24.svg';
-import IconQuestion24 from './24/icon-question-24.svg';
-import IconRedo24 from './24/icon-redo-24.svg';
-import IconReload24 from './24/icon-reload-24.svg';
-import IconRouble24 from './24/icon-rouble-24.svg';
-import IconSale24 from './24/icon-sale-24.svg';
-import IconSearch24 from './24/icon-search-24.svg';
-import IconSettings24 from './24/icon-settings-24.svg';
-import IconShare24 from './24/icon-share-24.svg';
-import IconTelegram24 from './24/icon-telegram-24.svg';
-import IconThumbDownS24 from './24/icon-thumb_down_s-24.svg';
-import IconThumbDown24 from './24/icon-thumb_down-24.svg';
-import IconThumbUpS24 from './24/icon-thumb_up_s-24.svg';
-import IconThumbUp24 from './24/icon-thumb_up-24.svg';
-import IconTicket24 from './24/icon-ticket-24.svg';
-import IconTwitter24 from './24/icon-twitter-24.svg';
-import IconUndo24 from './24/icon-undo-24.svg';
-import IconUnlock24 from './24/icon-unlock-24.svg';
-import IconVisibiliyOff24 from './24/icon-visibiliy_off-24.svg';
-import IconVisibiliyOn24 from './24/icon-visibiliy_on-24.svg';
-import IconVk24 from './24/icon-vk-24.svg';
-import IconWarning24 from './24/icon-warning-24.svg';
-import IconWebmoney24 from './24/icon-webmoney-24.svg';
-import IconWebsite24 from './24/icon-website-24.svg';
-import IconWidget24 from './24/icon-widget-24.svg';
-import IconYandexMoney24 from './24/icon-yandex-money-24.svg';
-import IconYoutube24 from './24/icon-youtube-24.svg';
-import IconMail24 from './24/icon-mail-24.svg';
-import IconMenuLeft24 from './24/icon-menu-left-24.svg';
-import IconMenuRight24 from './24/icon-menu-right-24.svg';
-import IconMessage24 from './24/icon-message-24.svg';
-import IconPan24 from './24/icon-pan-24.svg';
-import IconPlugPlug24 from './24/icon-plug-plug-24.svg';
-import IconPlugServices24 from './24/icon-plug-services-24.svg';
-import IconSwapHor24 from './24/icon-swap_hor-24.svg';
+import IconArrowDSolid16 from './16/icon_Arrow_D_solid_16.svg';
+import IconArrowRSolid16 from './16/icon_Arrow_R_solid_16.svg';
+import IconArrowLSolid16 from './16/icon_arrow_L_solid_16.svg';
+import IconArrowSortingDown16 from './16/icon_arrow_sorting_down_16.svg';
+import IconArrowSortingUp16 from './16/icon_arrow_sorting_up_16.svg';
+import IconArrowUSolid16 from './16/icon_Arrow_U_solid_16.svg';
+import IconBullet16 from './16/icon_bullet_16.svg';
+import IconCalendar16 from './16/icon_calendar_16.svg';
+import IconCheckmark16 from './16/icon_checkmark_16.svg';
+import IconCloseAlt16 from './16/icon_close_alt_16.svg';
+import IconDislikeOutline16 from './16/icon_dislike_outline_16.svg';
+import IconDrag16 from './16/icon_drag_16.svg';
+import IconEdit16 from './16/icon_edit_16.svg';
+import IconFile16 from './16/icon_file_16.svg';
+import IconFile216 from './16/icon_file2_16.svg';
+import IconHide16 from './16/icon_hide_16.svg';
+import IconInfo16 from './16/icon_info_16.svg';
+import IconLikeOutline16 from './16/icon_like_outline_16.svg';
+import IconLoading16 from './16/icon_loading_16.svg';
+import IconMinus16 from './16/icon_minus_16.svg';
+import IconOpenLink16 from './16/icon_open_link_16.svg';
+import IconPin16 from './16/icon_pin_16.svg';
+import IconPlus16 from './16/icon_plus_16.svg';
+import IconView16 from './16/icon_view_16.svg';
+import IconAttendees24 from './24/icon_attendees_24.svg';
+import IconDocument24 from './24/icon_document_24.svg';
+import IconHome24 from './24/icon_home_24.svg';
+import IconAddCalendar24 from './24/icon_add_calendar_24.svg';
+import IconAlert24 from './24/icon_alert_24.svg';
+import IconApplePay24 from './24/icon_apple_pay_24.svg';
+import IconArrowNW24 from './24/icon_arrow_NW_24.svg';
+import IconArrowTale24 from './24/icon_arrow_tale_24.svg';
+import IconArrowTaleCurve24 from './24/icon_arrow_tale_curve_24.svg';
+import IconArrowDown24 from './24/icon_arrow_down_24.svg';
+import IconArrowLeft24 from './24/icon_arrow_left_24.svg';
+import IconArrowRight24 from './24/icon_arrow_right_24.svg';
+import IconArrowUp24 from './24/icon_arrow_up_24.svg';
+import IconBell24 from './24/icon_bell_24.svg';
+import IconBookmarkS24 from './24/icon_bookmark_s_24.svg';
+import IconBookmark24 from './24/icon_bookmark_24.svg';
+import IconCalendar24 from './24/icon_calendar_24.svg';
+import IconCard24 from './24/icon_card_24.svg';
+import IconCheckCircle24 from './24/icon_check_circle_24.svg';
+import IconCheck24 from './24/icon_check_24.svg';
+import IconChevronDown24 from './24/icon_chevron_down_24.svg';
+import IconChevronLeft24 from './24/icon_chevron_left_24.svg';
+import IconChevronRight24 from './24/icon_chevron_right_24.svg';
+import IconChevronUp24 from './24/icon_chevron_up_24.svg';
+import IconCloseCircle24 from './24/icon_close_circle_24.svg';
+import IconClose24 from './24/icon_close_24.svg';
+import IconCopy24 from './24/icon_copy_24.svg';
+import IconDelete24 from './24/icon_delete_24.svg';
+import IconDrag24 from './24/icon_drag_24.svg';
+import IconEdit24 from './24/icon_edit_24.svg';
+import IconMore24 from './24/icon_more_24.svg';
+import IconEnterFullscreen24 from './24/icon_enter_fullscreen_24.svg';
+import IconEnter24 from './24/icon_enter_24.svg';
+import IconExitFullscreen24 from './24/icon_exit_fullscreen_24.svg';
+import IconFacebook24 from './24/icon_facebook_24.svg';
+import IconInfo24 from './24/icon_info_24.svg';
+import IconInstagram24 from './24/icon_instagram_24.svg';
+import IconLink24 from './24/icon_link_24.svg';
+import IconLoading24 from './24/icon_loading_24.svg';
+import IconLock24 from './24/icon_lock_24.svg';
+import IconLogin24 from './24/icon_login_24.svg';
+import IconLogout24 from './24/icon_logout_24.svg';
+import IconMenu24 from './24/icon_menu_24.svg';
+import IconMinusCircle24 from './24/icon_minus_circle_24.svg';
+import IconMinus24 from './24/icon_minus_24.svg';
+import IconPhoto24 from './24/icon_photo_24.svg';
+import IconPin24 from './24/icon_pin_24.svg';
+import IconPlayS24 from './24/icon_play_s_24.svg';
+import IconPlay24 from './24/icon_play_24.svg';
+import IconPlusCircle24 from './24/icon_plus_circle_24.svg';
+import IconPlus24 from './24/icon_plus_24.svg';
+import IconProfile24 from './24/icon_profile_24.svg';
+import IconQiwi24 from './24/icon_qiwi_24.svg';
+import IconQrcode24 from './24/icon_qrcode_24.svg';
+import IconQuestion24 from './24/icon_question_24.svg';
+import IconRedo24 from './24/icon_redo_24.svg';
+import IconReload24 from './24/icon_reload_24.svg';
+import IconRouble24 from './24/icon_rouble_24.svg';
+import IconSale24 from './24/icon_sale_24.svg';
+import IconSearch24 from './24/icon_search_24.svg';
+import IconSettings24 from './24/icon_settings_24.svg';
+import IconShare24 from './24/icon_share_24.svg';
+import IconTelegram24 from './24/icon_telegram_24.svg';
+import IconThumbDownS24 from './24/icon_thumb_down_s_24.svg';
+import IconThumbDown24 from './24/icon_thumb_down_24.svg';
+import IconThumbUpS24 from './24/icon_thumb_up_s_24.svg';
+import IconThumbUp24 from './24/icon_thumb_up_24.svg';
+import IconTicket24 from './24/icon_ticket_24.svg';
+import IconTwitter24 from './24/icon_twitter_24.svg';
+import IconUndo24 from './24/icon_undo_24.svg';
+import IconUnlock24 from './24/icon_unlock_24.svg';
+import IconVisibiliyOff24 from './24/icon_visibiliy_off_24.svg';
+import IconVisibiliyOn24 from './24/icon_visibiliy_on_24.svg';
+import IconVk24 from './24/icon_vk_24.svg';
+import IconWarning24 from './24/icon_warning_24.svg';
+import IconWebmoney24 from './24/icon_webmoney_24.svg';
+import IconWebsite24 from './24/icon_website_24.svg';
+import IconWidget24 from './24/icon_widget_24.svg';
+import IconYandexMoney24 from './24/icon_yandex_money_24.svg';
+import IconYoutube24 from './24/icon_youtube_24.svg';
+import IconMail24 from './24/icon_mail_24.svg';
+import IconMenuLeft24 from './24/icon_menu_left_24.svg';
+import IconMenuRight24 from './24/icon_menu_right_24.svg';
+import IconMessage24 from './24/icon_message_24.svg';
+import IconHand24 from './24/icon_hand_24.svg';
+import IconSocket24 from './24/icon_socket_24.svg';
+import IconServices24 from './24/icon_services_24.svg';
+import IconSwapHor24 from './24/icon_swap_hor_24.svg';
 
 export {
     IconArrowDSolid16,
     IconArrowRSolid16,
-    IconArrowSolid16,
+    IconArrowLSolid16,
     IconArrowSortingDown16,
     IconArrowSortingUp16,
     IconArrowUSolid16,
@@ -133,7 +132,6 @@ export {
     IconLikeOutline16,
     IconLoading16,
     IconMinus16,
-    IconModer16,
     IconOpenLink16,
     IconPin16,
     IconPlus16,
@@ -168,7 +166,7 @@ export {
     IconDelete24,
     IconDrag24,
     IconEdit24,
-    IconEllipsis24,
+    IconMore24,
     IconEnterFullscreen24,
     IconEnter24,
     IconExitFullscreen24,
@@ -222,8 +220,8 @@ export {
     IconMenuLeft24,
     IconMenuRight24,
     IconMessage24,
-    IconPan24,
-    IconPlugPlug24,
-    IconPlugServices24,
+    IconHand24,
+    IconSocket24,
+    IconServices24,
     IconSwapHor24,
 };
