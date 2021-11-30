296c52be30406aaf09ee5451bc4eb88018e93a86
Alexander Kostyuk <a.e.kostyuk@gmail.com>
2021-11-30 16:05:16 +0300

    PL-483 Исправлен ховер у List.Item для дропдауна второго уровня

--- src/components/popup/index.tsx
+++ src/components/popup/index.tsx
@@ -142,6 +142,7 @@ export const Popup = React.forwardRef<IPopupActions, IPopupProps>(
 
         useLayoutEffect(() => {
             if (isOpen) {
+                if (triggerRef.current) triggerRef.current.className = 'popup-hovered-trigger';
                 focusedElBeforeOpen.current = document.activeElement;
                 setPosition();
                 if (contentRef.current) {
@@ -149,6 +150,7 @@ export const Popup = React.forwardRef<IPopupActions, IPopupProps>(
                 }
                 handleLockScroll();
             } else {
+                if (triggerRef.current) triggerRef.current.className = '';
                 resetScroll();
             }
             return () => {
