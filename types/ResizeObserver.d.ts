c5ead443b2e7969545e6b8b3821433a6d7971774
Ilya Dostoynov <ilyadostoynov@gmail.com>
2022-03-03 13:54:06 +0300

    KV-44 проверка гипотезы. declare const ResizeObserver для тс

--- types/ResizeObserver.d.ts
+++ types/ResizeObserver.d.ts
@@ -2,3 +2,10 @@ declare const ResizeObserver: {
     prototype: ResizeObserver;
     new (callback: ResizeObserverCallback): ResizeObserver;
 };
+
+// eslint-disable-next-line @typescript-eslint/naming-convention
+export interface ResizeObserver {
+    disconnect(): void;
+    observe(target: Element, options?: ResizeObserverOptions): void;
+    unobserve(target: Element): void;
+}
