4e7b0fab242ce9865a54d4ff1468c09993f807e2
Ilya Dostoynov <ilyadostoynov@gmail.com>
2022-04-05 12:56:03 +0300

    KV-180 описано поведение PageHeader для основных случаев.

--- src/components/pageheader/PageHeader.stories.tsx
+++ src/components/pageheader/PageHeader.stories.tsx
@@ -2,22 +2,53 @@ import {Meta} from '@storybook/react/types-6-0';
 import {PageHeader} from './index';
 import {IStorybookComponent} from '../../services/helpers/storyBookHelpers';
 import React from 'react';
-import Typography from '../typography/Typography';
+import {IPageHeaderProps} from './PageHeader';
+import {Button, ButtonVariant} from 'index';
 
 export default {
     title: 'Page Header',
     component: PageHeader,
 } as Meta;
 
-export const Test3: IStorybookComponent = () => {
+const MainButton = <Button>Main button</Button>;
+const SecondaryButton = <Button variant={ButtonVariant.secondary}>Secondary button</Button>;
+const Label = 'Page Header';
+const Caption = 'Status text';
+const onBackClick = () => {
+    window.alert('Back button is cliked');
+};
+
+const HeaderVariansWithoutBack: IPageHeaderProps[] = [
+    {label: Label},
+    {label: Label, children: [MainButton]},
+    {label: Label, children: [SecondaryButton, MainButton]},
+    {label: Label, caption: Caption, children: [SecondaryButton, MainButton]},
+    {label: Label, caption: Caption},
+];
+
+const HeaderVariansWithBack: IPageHeaderProps[] = [
+    {label: Label, children: [SecondaryButton, MainButton], onBackClick},
+    {label: Label, caption: Caption, children: [SecondaryButton, MainButton], onBackClick},
+    {label: Label, onBackClick},
+    {label: Label, caption: Caption, onBackClick},
+];
+
+export const WithoutBackButton: IStorybookComponent = () => {
     return (
-        <PageHeader label="Page Header" caption="Status text">
-            {/*<Button variant={ButtonVariant.secondary}>Secondary button</Button>*/}
-            {/*<Button>Main button</Button>*/}
-        </PageHeader>
+        <>
+            {HeaderVariansWithoutBack.map((el, index) => (
+                <PageHeader key={index} {...el} />
+            ))}
+        </>
     );
 };
 
-export const Test2: IStorybookComponent = () => {
-    return <PageHeader label={<Typography variant="lead">Page Header</Typography>} />;
+export const WithBackButton: IStorybookComponent = () => {
+    return (
+        <>
+            {HeaderVariansWithBack.map((el, index) => (
+                <PageHeader key={index} {...el} />
+            ))}
+        </>
+    );
 };
--- src/components/pageheader/PageHeader.tsx
+++ src/components/pageheader/PageHeader.tsx
@@ -2,8 +2,10 @@ import React, {FC, ReactElement, useMemo} from 'react';
 import Typography, {ITypographyProps} from '../typography/Typography';
 import './index.less';
 import {Gap} from '../gap';
+import {Button, ButtonVariant} from '../button';
+import ArrowBack from '../../assets/svg/24/icon-arrow-tale-24.svg';
 
-interface IPageHeaderProps {
+export interface IPageHeaderProps {
     // children?: Array<typeof Button>;
     children?: any;
     label: string | ReactElement<ITypographyProps, typeof Typography>;
@@ -21,16 +23,19 @@ const PageHeader: FC<IPageHeaderProps> = ({children, label, caption, onBackClick
     }, [onBackClick]);
 
     const Label = useMemo(() => {
+        const styles = isTall ? {padding: '5px 0 16px'} : undefined;
         if (typeof label === 'string') {
             return (
-                <Typography variant="body" fontWeight="bold">
+                <Typography noPadding={!isTall} variant="body" fontWeight="bold" style={styles}>
                     {label}
                 </Typography>
             );
         } else {
-            return label;
+            const Label = label;
+            Label.props.style = styles;
+            return Label;
         }
-    }, [label]);
+    }, [label, isTall]);
 
     if (!isTall) {
         return (
@@ -39,7 +44,7 @@ const PageHeader: FC<IPageHeaderProps> = ({children, label, caption, onBackClick
                 <div className="lflex lflex--align-centered">
                     {caption && (
                         <>
-                            <Typography className="cpageheader__caption t-color-gray-50" variant="caption">
+                            <Typography noPadding className="cpageheader__caption t-color-gray-50" variant="caption">
                                 {caption}
                             </Typography>
                             {children && <Gap size={0.5} />}
@@ -56,7 +61,29 @@ const PageHeader: FC<IPageHeaderProps> = ({children, label, caption, onBackClick
         );
     }
 
-    return <div>dasdas</div>;
+    return (
+        <div>
+            <div className="cpageheader">
+                {onBackClick && (
+                    <Button onClick={onBackClick} variant={ButtonVariant.transparent} icon={<ArrowBack />} />
+                )}
+                <div className="lflex lflex--align-centered">
+                    {React.Children.toArray(children).map((button) => (
+                        <>
+                            <Gap size={0.5} />
+                            {button}
+                        </>
+                    ))}
+                </div>
+            </div>
+            {caption && (
+                <Typography className="t-color-gray-50" variant="caption">
+                    {caption}
+                </Typography>
+            )}
+            {Label}
+        </div>
+    );
 };
 
 export default PageHeader;
