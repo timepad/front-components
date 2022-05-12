4dcf1209048ec1d6e69278f3273a430d57d8892d
Ilya Dostoynov <ilyadostoynov@gmail.com>
2022-05-12 21:24:21 +0300

    TPI-341 вынес RoundedCheckbox, удалил формы

--- src/components/form/Checkbox/RoundedCheckbox.stories.tsx
+++ src/components/form/Checkbox/RoundedCheckbox.stories.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
 
 import {Meta} from '@storybook/react/types-6-0';
-import {RadioButton} from './index';
 import {IStorybookComponent, StoryTitle} from '../../../services/helpers/storyBookHelpers';
 
 import 'css/bundle.less';
-import '../storybook/demo.less';
+import './storybook/demo.less';
+import {Form} from '../Form';
 
 export default {
-    title: 'RadioButton',
-    component: RadioButton,
+    title: 'RoundedCheckbox',
+    component: Form.Checkbox,
 } as Meta;
 
 const themes = {
@@ -43,11 +43,9 @@ interface IInputsContainerProps {
     themeColor: keyof typeof themes;
 }
 
-const RadioButtonContainer = (props: IInputsContainerProps) => {
+const RoundedCheckboxContainer = (props: IInputsContainerProps) => {
     const [checkedRadioOne, setCheckedRadioOne] = React.useState(false);
-    const [checkedRadioOneLabeled, setCheckedRadioOneLabeled] = React.useState(false);
     const [checkedRadioTwo, setCheckedRadioTwo] = React.useState(true);
-    const [checkedRadioTwoLabeled, setCheckedRadioTwoLabeled] = React.useState(true);
     const {themeColor} = props;
 
     return (
@@ -59,52 +57,25 @@ const RadioButtonContainer = (props: IInputsContainerProps) => {
                     <div>
                         <div className="t-lead t-lead-24">{themes[themeColor].title}</div>
                         <div className="inputs-container lfelx-y-axis">
-                            <ComponentBlock title="Button icon">
+                            <ComponentBlock title="Rounded checkbox">
                                 <div className="lbrick-2" />
-                                <RadioButton
+                                <Form.Checkbox
+                                    rounded
                                     value="s"
                                     checked={checkedRadioOne}
                                     onChange={() => setCheckedRadioOne(!checkedRadioOne)}
                                 />
                                 <div className="lbrick-2" />
-                                <RadioButton value="s" disabled />
+                                <Form.Checkbox rounded value="s" disabled />
                                 <div className="lbrick-2" />
-                                <RadioButton
+                                <Form.Checkbox
+                                    rounded
                                     value="s"
                                     checked={checkedRadioTwo}
                                     onChange={() => setCheckedRadioTwo(!checkedRadioTwo)}
                                 />
                                 <div className="lbrick-2" />
-                                <RadioButton value="s" checked disabled />
-                                <div className="lbrick-2" />
-                            </ComponentBlock>
-                            <ComponentBlock title="Button text">
-                                <div className="lbrick-2" />
-                                <div>
-                                    <RadioButton
-                                        value="s"
-                                        checked={checkedRadioOneLabeled}
-                                        label="Auto Active"
-                                        onChange={() => setCheckedRadioOneLabeled(!checkedRadioOneLabeled)}
-                                    />
-                                </div>
-                                <div className="lbrick-2" />
-                                <div>
-                                    <RadioButton value="s" disabled label="Auto Disabled" />
-                                </div>
-                                <div className="lbrick-2" />
-                                <div>
-                                    <RadioButton
-                                        value="s"
-                                        checked={checkedRadioTwoLabeled}
-                                        label="Auto Active"
-                                        onChange={() => setCheckedRadioTwoLabeled(!checkedRadioTwoLabeled)}
-                                    />
-                                </div>
-                                <div className="lbrick-2" />
-                                <div>
-                                    <RadioButton value="s" checked disabled label="Auto Disabled" />
-                                </div>
+                                <Form.Checkbox rounded value="s" checked disabled />
                                 <div className="lbrick-2" />
                             </ComponentBlock>
                         </div>
@@ -122,7 +93,7 @@ export const Simple: IStorybookComponent = () => {
         <>
             <StoryTitle>Primary</StoryTitle>
             {Object.keys(themes).map((themeColor, index) => (
-                <RadioButtonContainer themeColor={themeColor as keyof typeof themes} key={index} />
+                <RoundedCheckboxContainer themeColor={themeColor as keyof typeof themes} key={index} />
             ))}
         </>
     );
