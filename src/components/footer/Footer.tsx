1e19af6099ef722691493bb12be9ff664dd3b6ba
Ilya Dostoynov <ilyadostoynov@gmail.com>
2022-07-13 14:06:52 +0300

    ACT-657 moved default links

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -8,11 +8,30 @@ import {FooterSocials} from './FooterSocials';
 import {FooterCopyright} from './FooterCopyright';
 import {FooterLinks, IFooterLinksItem} from './FooterLinks';
 
+const defaultLinks: Array<IFooterLinksItem> = [
+    {
+        title: 'Афиша событий',
+        items: ['Найти событие', 'Рекомендуемое', 'Подписки'],
+    },
+    {
+        title: 'Организаторам',
+        items: ['Создать событие', 'Возможности', 'Тарифы', 'Реклама'],
+    },
+    {
+        title: 'Timepad',
+        items: ['О нас', 'Блог', 'Вакансии', 'Контакты', 'Документы'],
+    },
+    {
+        title: 'Помощь',
+        items: ['Задать вопрос', 'База знаний', 'Разработчикам'],
+    },
+];
+
 interface IFooterProps {
-    links: Array<IFooterLinksItem>;
+    links?: Array<IFooterLinksItem>;
 }
 
-export const Footer: React.FC<IFooterProps> = ({links}) => {
+export const Footer: React.FC<IFooterProps> = ({links = defaultLinks}) => {
     return (
         <div className={component('footer')()}>
             <div className="lpage">
