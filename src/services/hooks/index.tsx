6d1dc6d02570ba0bf10ecc1077f0e42c668ed15d
Ilya Dostoynov <ilyadostoynov@gmail.com>
2023-01-17 21:22:30 +0300

    ACT-657 moved useScript form ntp

--- src/services/hooks/index.tsx
+++ src/services/hooks/index.tsx
@@ -2,5 +2,6 @@ import {useClientRect} from './useClientRect';
 import {useClickOutside} from './useClickOutside';
 import useEvent from './useEvent';
 import {useMedia} from './useMedia';
+import {useScript} from './useScript';
 
-export {useClientRect, useClickOutside, useEvent, useMedia};
+export {useClientRect, useClickOutside, useEvent, useMedia, useScript};
