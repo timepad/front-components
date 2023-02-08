a9a3c60ee734bd69867a1961aed357991e666fbb
Ilya Dostoynov <ilyadostoynov@gmail.com>
2023-02-08 14:29:18 +0300

    RET-464 edit Typography.Multiple to container

--- src/components/typography/TypographyMultiple.tsx
+++ src/components/typography/TypographyMultiple.tsx
@@ -1,32 +1,10 @@
-import React, {FC, ReactNode} from 'react';
+import React, {FC, PropsWithChildren} from 'react';
 import Typography, {ITypographyCommonProps} from './Typography';
-import {component} from '../../services/helpers/classHelpers';
 
-interface ITypographyMultiple extends ITypographyCommonProps {
-    afisha?: boolean;
-    text: ReactNode;
-    caption: ReactNode;
-    reverse?: boolean;
-}
-
-export const TypographyMultiple: FC<ITypographyMultiple> = ({afisha, text, caption, reverse = false, ...props}) => {
-    // TODO: добавлен флаг afisha, который специально оверайдит стили для афишы
-
-    const textClassNames = component('typography__multiple', 'text')({afisha});
-    const captionClassNames = component('typography__multiple', 'caption')({afisha});
-
-    const content = [
-        <div key="key_text" className={textClassNames}>
-            {text}
-        </div>,
-        <div key="key_caption" className={captionClassNames}>
-            {caption}
-        </div>,
-    ];
-
+export const TypographyMultiple: FC<PropsWithChildren<ITypographyCommonProps>> = ({children, ...props}) => {
     return (
-        <Typography {...props} variant="multiple" reverse={reverse}>
-            {!reverse ? content.map((component) => component) : content.reverse().map((component) => component)}
+        <Typography {...props} variant="multiple">
+            {children}
         </Typography>
     );
 };
