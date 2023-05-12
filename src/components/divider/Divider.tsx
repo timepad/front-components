8a228dc9e4e68ed8d6e879028f46f8cb6eba52dd
andrew9488 <pashkevichandrew@gmail.com>
2023-05-12 13:08:41 +0300

    TPI-580 Expand Divider component.

--- src/components/divider/Divider.tsx
+++ src/components/divider/Divider.tsx
@@ -6,10 +6,37 @@ import './index.less';
 interface IProps {
     className?: string;
     as?: keyof JSX.IntrinsicElements;
+    vertical?: boolean;
+    margin?: number | [number, number];
 }
 
-export const Divider: React.FC<React.PropsWithChildren<IProps>> = ({className, as = 'div', ...props}) => {
+export const Divider: React.FC<React.PropsWithChildren<IProps>> = ({
+    className,
+    as = 'div',
+    vertical = false,
+    margin = 0,
+    ...props
+}) => {
     const Tag = as;
-    const rowClass = cx(component('list-divider')(), className);
-    return <Tag className={rowClass} {...props} />;
+    const styles = {
+        margin: getMargin(margin),
+    };
+    const rowClass = cx(
+        component('list-divider')(),
+        {
+            vertical,
+        },
+        className,
+    );
+    return <Tag className={rowClass} style={styles} {...props} />;
+};
+
+const getMargin = (margin: number | [number, number]) => {
+    if (!margin) return;
+    if (typeof margin === 'number') {
+        return `${margin}px`;
+    }
+    if (Array.isArray(margin)) {
+        return margin.reduce((acc, el) => acc + ` ${el}px`, '');
+    }
 };
