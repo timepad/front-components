26fd531287cf4b23a5e06830e7a7a6366303172e
andrew9488 <pashkevichandrew@gmail.com>
2023-05-15 14:42:54 +0300

    TPI-580 Add examples to storybook

--- src/components/divider/Divider.stories.tsx
+++ src/components/divider/Divider.stories.tsx
@@ -25,3 +25,41 @@ export const Default: IStorybookComponent = () => {
         </Modules>
     );
 };
+
+export const VerticalWithMargin: IStorybookComponent = () => {
+    return (
+        <Modules>
+            <Modules.M>
+                <div style={{display: 'flex'}}>
+                    <div>Divider имеет левый и</div>
+                    <Divider vertical margin={[0, 5]} />
+                    <div>правый margin по 5px</div>
+                </div>
+            </Modules.M>
+        </Modules>
+    );
+};
+
+export const DefaultWithMargin: IStorybookComponent = () => {
+    return (
+        <Modules>
+            <Modules.M>
+                <div>Divider имеет верхний и</div>
+                <Divider margin={[5, 0]} />
+                <div>нижний margin по 5px</div>
+            </Modules.M>
+        </Modules>
+    );
+};
+
+export const VerticalAsSpan: IStorybookComponent = () => {
+    return (
+        <Modules>
+            <Modules.M>
+                <span>Divider обозначен через тег span</span>
+                <Divider vertical as="span" margin={5} />
+                <span>и имеет margin 5px</span>
+            </Modules.M>
+        </Modules>
+    );
+};
