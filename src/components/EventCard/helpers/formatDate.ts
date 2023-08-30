fa07675057b2725518791801859274fbb8ec22ff
andrew9488 <pashkevichandrew@gmail.com>
2023-08-30 11:54:08 +0300

    DEV-196 check end date of event in formatEventDate function

--- src/components/EventCard/helpers/formatDate.ts
+++ src/components/EventCard/helpers/formatDate.ts
@@ -1,6 +1,7 @@
 import moment from 'moment';
 
-export const formatEventDate = (start: Date, end?: Date, isSession = false): string => {
+export const formatEventDate = (start: Date, end?: Date, isSession?: boolean): string => {
     const startEventDate = isSession ? moment(start).format('ddd, D MMMM H:mm') : moment(start).format('D MMMM H:mm');
-    return `${startEventDate} – ${moment(end).format('H:mm')}`;
+    const endEventDate = end ? ` – ${moment(end).format('H:mm')}` : '';
+    return `${startEventDate}${endEventDate}`;
 };
