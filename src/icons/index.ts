fe9a66548f99848b0fc40a5fab8a159054060afd
petrzhirkov <zhirkovpetr95@gmail.com>
2025-02-04 15:30:30 +0300

    DEV-898 исправил экспорт в index файле с иконками

--- src/icons/index.ts
+++ src/icons/index.ts
@@ -1,163 +1,28 @@
 // 16
-export {
-    IconAdmin16,
-    IconArrowDown16,
-    IconArrowDownSolid16,
-    IconArrowLeftSolid16,
-    IconArrowRightSolid16,
-    IconArrowSortingDown16,
-    IconArrowSortingUp16,
-    IconArrowUpSolid16,
-    IconBullet16,
-    IconCalendar16,
-    IconCheckmark16,
-    IconCloseAlt16,
-    IconCompleteFile16,
-    IconDislikeOutline16,
-    IconDrag16,
-    IconEdit16,
-    IconEmptyFile16,
-    IconInfo16,
-    IconLikeOutline16,
-    IconLoading16,
-    IconMinus16,
-    IconModer16,
-    IconModeration16,
-    IconOpenLink16,
-    IconPause16,
-    IconPin16,
-    IconPlus16,
-    IconSearch16,
-    IconStop16,
-    IconVisibilityOff16,
-    IconVisibilityOn16,
-} from '../assets/svg/16';
+export * from '../assets/svg/16';
 
 // 24
-export {
-    IconAddCalendar24,
-    IconAlert24,
-    IconArrowDown24,
-    IconArrowDownSolid24,
-    IconArrowLeft24,
-    IconArrowLeftSolid24,
-    IconArrowNW24,
-    IconArrowRight24,
-    IconArrowRightSolid24,
-    IconArrowTale24,
-    IconArrowTaleCurve24,
-    IconArrowUp24,
-    IconArrowUpSolid24,
-    IconAttendees24,
-    IconBell24,
-    IconBookmark24,
-    IconBookmarkSolid24,
-    IconCalendar24,
-    IconCard24,
-    IconCheck24,
-    IconCheckCircle24,
-    IconClose24,
-    IconCloseCircle24,
-    IconCopy24,
-    IconDelete24,
-    IconDocument24,
-    IconDrag24,
-    IconEdit24,
-    IconEnter24,
-    IconEnterFullscreen24,
-    IconExitFullscreen24,
-    IconHand24,
-    IconHome24,
-    IconInfo24,
-    IconLink24,
-    IconLoading24,
-    IconLock24,
-    IconLogin24,
-    IconLogout24,
-    IconMail24,
-    IconMenu24,
-    IconMenuLeft24,
-    IconMenuRight24,
-    IconMinus24,
-    IconMinusCircle24,
-    IconMore24,
-    IconMessage24,
-    IconPhoto24,
-    IconPin24,
-    IconPlay24,
-    IconPlaySolid24,
-    IconPlus24,
-    IconPlusCircle24,
-    IconProfile24,
-    IconQrcode24,
-    IconQuestion24,
-    IconRedo24,
-    IconReload24,
-    IconRouble24,
-    IconSale24,
-    IconSearch24,
-    IconServices24,
-    IconSettings24,
-    IconShare24,
-    IconSocket24,
-    IconSwapHor24,
-    IconThumbDown24,
-    IconThumbDownSolid24,
-    IconThumbUp24,
-    IconThumbUpSolid24,
-    IconTicket24,
-    IconUndo24,
-    IconUnlock24,
-    IconVisibilityOff24,
-    IconVisibilityOn24,
-    IconWarning24,
-    IconWebsite24,
-    IconWidget24,
-} from '../assets/svg/24';
+export * from '../assets/svg/24';
 
 // map
-export {Pin64} from '../assets/svg/map';
+export * from '../assets/svg/map';
 
 // misc
-export {IconPlayVideo} from '../assets/svg/misc';
+export * from '../assets/svg/misc';
 
 // vendor
 
 // appShops
-export {IconAppStore} from '../assets/svg/vendor/appShops';
-export {IconGooglePlay} from '../assets/svg/vendor/appShops';
+export * from '../assets/svg/vendor/appShops';
 
 // bank
-export {
-    IconBankAlfa,
-    IconBankAlfaWhite,
-    IconBankGazprom,
-    IconBankGazpromWhite,
-    IconBankOtkritie,
-    IconBankOtkritieWhite,
-    IconBankPochta,
-    IconBankPromsvyaz,
-    IconBankPromsvyazWhite,
-    IconBankRaiffeisen,
-    IconBankRosbank,
-    IconBankSber,
-    IconBankSberWhite,
-    IconBankTinkoff,
-    IconBankVTB,
-} from '../assets/svg/vendor/bank';
+export * from '../assets/svg/vendor/bank';
 
 // logo
-export {Logo, SumsubLogo} from '../assets/svg/vendor/logo';
+export * from '../assets/svg/vendor/logo';
 
 // paymentSystems
-export {IconApplePay, IconQiwi, IconWebmoney, IconYandexMoney} from '../assets/svg/vendor/paymentSystems';
+export * from '../assets/svg/vendor/paymentSystems';
 
 // socialMedia
-export {
-    IconFacebook,
-    IconInstagram,
-    IconTelegram,
-    IconTwitter,
-    IconVk,
-    IconYoutube,
-} from '../assets/svg/vendor/socialMedia';
+export * from '../assets/svg/vendor/socialMedia';
