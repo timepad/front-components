0974a999322f748d9861c98af35b7f33ea264221
andrew9488 <pashkevichandrew@gmail.com>
2026-06-19 16:47:36 +0300

    dev-1894 правки после ревью

--- src/services/helpers/addQaTagsToChildren/addQaTagsToChildren.ts
+++ src/services/helpers/addQaTagsToChildren/addQaTagsToChildren.ts
@@ -3,7 +3,7 @@ import React from 'react';
 export const addQaTagsToChildren = (children: React.ReactNode, getAttr: (index: number) => string): React.ReactNode => {
     return React.Children.map(children, (child, index) => {
         return React.isValidElement(child)
-            ? React.cloneElement(child, {...child.props, 'data-qa': getAttr(index)})
+            ? React.cloneElement(child, {'data-qa': getAttr(index), ...child.props})
             : child;
     });
 };
