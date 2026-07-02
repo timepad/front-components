036b2d379ead5a68ece6ee8feeab99e98af06754
Alexander Kostyuk <a.e.kostyuk@gmail.com>
2026-07-02 15:26:00 +0300

    DEV-2045 Правка экспортов

--- src/components/fileuploader/index.ts
+++ src/components/fileuploader/index.ts
@@ -15,3 +15,16 @@ export type {
     FileUploaderViewMode,
     FileUploaderUploadStrategy,
 } from './FileUploader.types';
+export {createFilePresignUploadStrategy, createFileUploaderPresignPayload} from './FileUploader.presign';
+export type {
+    FileUploaderPassportScanKind,
+    FileUploaderPresignIntent,
+    FileUploaderPresignOptionValue,
+    ICreateFilePresignUploadStrategyOptions,
+    ICreateFileUploaderPresignPayloadOptions,
+    IFileUploaderPresignPayload,
+    IFileUploaderPresignResponse,
+    IFileUploaderPresignSession,
+    IFileUploaderS3UploadParameters,
+    IFileUploaderS3UploadStrategy,
+} from './FileUploader.presign';
